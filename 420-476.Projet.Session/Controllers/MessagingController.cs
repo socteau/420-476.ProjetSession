5f2ec609660ef853663574f7019cc37a825017ee
Samuel Octeau <Samuel Octeau>
2016-05-04 22:43:21 -0400

    no message

---
+++ 420-476.Projet.Session/Controllers/MessagingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _420_476.Projet.Session.Controllers
+{
+    public class MessagingController : Controller
+    {
+        // GET: Messaging
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
