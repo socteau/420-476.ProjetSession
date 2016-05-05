48d42009ef4dab580f3d56e6f0fdb0686bb4a006
Marc-Andre Nadon <mark_maerk@hotmail.com>
2016-05-05 14:06:23 -0400

    test

---
+++ 420-476.Projet.Session/Controllers/GestionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _420_476.Projet.Session.Controllers
+{
+    public class GestionController : Controller
+    {
+        public ActionResult Rapport()
+        {
+            return View();
+        }
+
+        public ActionResult GererProfils()
+        {
+            return View();
+        }
+
+        // GET: Gestion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
