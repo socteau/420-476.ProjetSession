9f4e4dd37752d831ff84732defb772238a21d153
Marc-Andre Nadon <mark_maerk@hotmail.com>
2016-05-04 20:24:26 -0400

    missing files and some others...

--- 420-476.Projet.Session/Models/Role.cs
+++ 420-476.Projet.Session/Models/Role.cs
@@ -6,7 +6,7 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-
+//test
 namespace _420_476.Projet.Session.Models
 {
     using System;
